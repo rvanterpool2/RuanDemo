321e421c3f5a4416f11fe38442121d63e4357b51
Ruan <rvanterpool2@student.gsu.edu>
2017-01-30 19:34:44 -0500

    second commit

--- app/src/main/java/ruan/ruandemo/ListViewActivity.java
+++ app/src/main/java/ruan/ruandemo/ListViewActivity.java
@@ -2,11 +2,14 @@ package ruan.ruandemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import ruan.ruandemo.Adapter.ListViewAdapter;
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,13 @@ public class ListViewActivity extends AppCompatActivity {
         listView= (ListView) findViewById(R.id.List_view);
         ListViewAdapter listViewAdapter= new ListViewAdapter(this);
         listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(this);
     }
 
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Toast.makeText(this,"listView was clicked at position"+position , Toast.LENGTH_LONG);
+    }
 }
